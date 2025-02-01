c54e27f9dfccd9e0a1c1a7c16232dbde9300b81e
iak-404 <d4k189@outlook.de>
2025-02-01 10:02:48 +0100

    About überarbeitet, css Dateien überarbeitet, Projects.js gestartet

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,13 @@
 import React from "react";
+import '../styles/projects.css';
 
 const Projects = () => {
     return(
-        <div>
-            <h1>Willkommen auf der About Seite!</h1>
-            <p>Die ist die Hauptseite der Anwendung.</p>
+        <div className="projects">
+            <div className="text">
+                <h2>Willkommen auf der Projekt Seite!</h2>
+                <p>Hier wird es eine Übersicht meiner Projekte geben.</p>
+            </div>
         </div>
     );
 };
